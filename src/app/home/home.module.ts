3d99caa18390747d33ffc6893e26fc4b1f241ad4
LucilaAllende <abigail_lucii@hotmail.com>
2024-03-07 10:00:29 -0300

    add swiper module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
@@ -16,6 +16,8 @@ import { SharedDirectivesModule } from '../directives/shared-directives.module';
     HomePageRoutingModule,
     SharedDirectivesModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
 })
 export class HomePageModule {}
