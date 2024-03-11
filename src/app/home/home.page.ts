3fd6be14ab3bef3221db4c9b7ea57d04e2a2d7ba
LucilaAllende <abigail_lucii@hotmail.com>
2024-03-11 08:56:30 -0300

    add data fake

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { categories } from 'src/assets/data/home';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  categories: any = categories;
+	highlights: any = [];
+	featured: any = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+/* 		this.http
+			.get('https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json')
+			.subscribe((res: any) => {
+        console.log(res);
+				this.categories = res.categories;
+				this.highlights = res.highlights;
+				this.featured = res.featured;
+			}); */
+			const res = {
+				"highlights": [
+					{
+						"id": 1,
+						"title": "Special Offer",
+						"description": "Get 20% off on selected items!",
+						"image": "offer.jpg"
+					},
+					{
+						"id": 2,
+						"title": "New Arrivals",
+						"description": "Discover our latest menu additions!",
+						"image": "new.jpg"
+					}
+				],
+				"featured": [
+					{
+						"id": 1,
+						"name": "Margherita Pizza",
+						"description": "Classic Italian pizza topped with tomatoes, mozzarella, and basil.",
+						"price": 10.99,
+						"image": "margherita.jpg"
+					},
+					{
+						"id": 2,
+						"name": "Cheeseburger",
+						"description": "Juicy beef patty topped with melted cheese, lettuce, and tomato.",
+						"price": 8.99,
+						"image": "cheeseburger.jpg"
+					}
+				]
+			}
+			this.highlights = res.highlights;
+			this.featured = res.featured;
+	}
 
 }
---
+++ src/assets/data/home.ts
@@ -0,0 +1,58 @@
+export const categories = [
+  {
+    text: 'Restaurants',
+    img: './c1.png',
+  },
+  {
+    text: 'Grocery',
+    img: './c2.png',
+  },
+  {
+    text: 'Offers',
+    img: './c3.png',
+  },
+  {
+    text: 'Pickup',
+    img: './c4.png',
+  },
+  {
+    text: 'HOP',
+    img: './c5.png',
+  },
+  {
+    text: 'Pharmacy',
+    img: './c6.png',
+  },
+];
+
+export const restaurants = [
+  {
+    name: 'Vapiano',
+    rating: '4.5 Excellent',
+    ratings: '(500+)',
+    distance: '0.7 miles away',
+    img: './r1.jpeg',
+    tags: ['Italian', 'Pizza', 'Pasta', 'Salads'],
+    duration: '35 - 45',
+  },
+  {
+    name: '✨Urban Greens✨',
+    id: '2',
+    rating: '4.9 Excellent',
+    ratings: '(500+)',
+    distance: '1.7 miles away',
+    img: './r2.jpeg',
+    tags: ['Salads', 'Vegan', 'Healthy', 'British'],
+    duration: '15 - 30',
+  },
+  {
+    name: 'El Minero',
+    id: '3',
+    rating: '4.5 Excellent',
+    ratings: '(500+)',
+    distance: '3 miles away',
+    img: './r3.jpeg',
+    tags: ['Spanish', 'Salads', 'Tpas', 'Pasta'],
+    duration: '25 - 45',
+  },
+];
